75984df0b5e7f964e0b31a59e3e4c522b3b21269
namda-on <jinyoung1997@gmail.com>
2021-06-01 22:02:41 +0900

    feat : login page 에서 버튼바꾸기

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,22 +1,44 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 import Logo4M from "../material/LOGO4_mob.png";
-import Logout from "../material/logout.png";
 import "../stylesheet/header.scss";
 const Header = () => {
+  const history = useHistory();
+  const [isLoginPage, setIsLoginPage] = useState(false);
+  useEffect(() => {
+    history.listen((location) => {
+      const isLoginPage = location.pathname === "/login";
+
+      setIsLoginPage(isLoginPage);
+    });
+  }, [history]);
+
+  const showAlert = () => {
+    alert("서비스 준비중입니다.");
+  };
+
   return (
     <div className="header">
       <Link to="/">
         <img className="header-logo" src={Logo4M} alt="logo" />
       </Link>
-      <Link to="/login">
-        <div className="logout-button">
-          {/* <img src={Logout} alt="logout" /> */}
-          <div className="box">
-            <p>로그아웃</p>
+      {isLoginPage ? (
+        <div class="sign-up" onClick={showAlert}>
+          <div className="logout-button">
+            <div className="box">
+              <p>회원가입</p>
+            </div>
           </div>
         </div>
-      </Link>
+      ) : (
+        <Link to="/login">
+          <div className="logout-button">
+            <div className="box">
+              <p>로그아웃</p>
+            </div>
+          </div>
+        </Link>
+      )}
     </div>
   );
 };
