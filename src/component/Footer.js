9548cf867e60628df38b602b2ddd79c0604ea1c7
DODO <thsehgml37@korea.ac.kr>
2021-09-07 23:38:14 +0900

    Fix: footer changed

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -4,7 +4,6 @@ import "../stylesheet/footer.scss";
 const Footer = () => {
   return (
     <div className="footer">
-      <p>Designed by SW ENGINEER DOHUI SON .</p>
       <h4>Copyright &copy; 2021 AI-MASK-BIN All rights reserved . </h4>
     </div>
   );
